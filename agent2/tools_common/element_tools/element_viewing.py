708444dd3fb78c7c62a4b2860cc1772919a8b105
Green0-0 <None>
2025-03-12 05:37:42 -0400

    fixes

--- agent2/tools_common/element_tools/element_viewing.py
+++ agent2/tools_common/element_tools/element_viewing.py
@@ -167,7 +167,7 @@ def view_element(state: AgentState, settings: ToolSettings, path: str, identifie
     if len(element.content.splitlines()) < settings.max_view_lines_start + settings.max_view_lines_end:
         content = element.to_string(number_lines=settings.number_lines, unindent_text=settings.unindent_inputs, mask_subelements = False)
         if len(element.elements) > 1:
-            content += f"\n**This element has {len(element.elements)} sub-elements, you can use the view_element tool to view them, just make sure to specify the entire path, for instance, {element.elements[0].identifier} or {element.elements[1].identifier}.**"
+            content += f"\n**This element has {len(element.elements)} sub-elements, you can use the open_element tool to edit them, just make sure to specify the entire path, for instance, {element.elements[0].identifier} or {element.elements[1].identifier}.**"
         return (f"Element {element.identifier} at {file.path}:\n{content}", None, None)
     content = element.to_string(number_lines=settings.number_lines, unindent_text=settings.unindent_inputs)
     lines = content.splitlines()
